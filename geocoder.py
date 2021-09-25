8d611e2c6dff5442b47bbd6e8dc5f3167da47d82
MatveyMatveyIl <matvey.ilichev@gmail.com>
2021-09-25 21:43:39 +0500

    pars cli input to argparser

--- geocoder.py
+++ geocoder.py
@@ -1,11 +1,14 @@
-import sys
-import re
 from utils.input_parser import address_parser
+from utils.arg_parser import init_parser
 
 
-def run(input_data):
-    address = address_parser(input_data)
+def run():
+    args = init_parser()
+    if args.address:
+        address = address_parser(args.address)
+    elif args.download:
+        city_name = args.download
 
 
 if __name__ == '__main__':
-    run(sys.argv[1:])
+    run()
---
+++ utils/arg_parser.py
@@ -0,0 +1,25 @@
+import argparse
+
+
+def init_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-address', '-a',
+                        dest='address',
+                        type=str,
+                        nargs='?',
+                        action='store',
+                        required=False,
+                        help='Адрес в формате: .'
+                             '"область=обл."_'
+                             '"город=гор.=г."_'
+                             '"улица=ул."_'
+                             '"дом=д."_'
+                             '"разделители: ; , /"')
+    parser.add_argument('-d', '-download',
+                        dest='download',
+                        type=str,
+                        action='store',
+                        required=False,
+                        help='')
+    args = parser.parse_args()
+    return args
